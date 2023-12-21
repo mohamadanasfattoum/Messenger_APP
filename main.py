f7a8e05d4b5992c20f552f139ff5c4b46cb8416e
Mohamad anas fattoum <samaanas30@gmail.com>
2023-12-21 02:03:25 +0100

    add db.db

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
-# save this as app.py
 from flask import Flask
-from flask_SQLAlchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Message(db.Model): # database table
-    id = db.Column(db.Integar, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(200), nullable=True)
     content = db.Column(db.String(1000), nullable=True)
-    created_at = db.Column(db.DateTime(), default= datetime.utcnow)
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
 
 
 
@@ -24,6 +23,8 @@ def start_page():
     return "Hello, World!"
 
 
-
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all() # to creat database
+
     app.run(debug=True)
